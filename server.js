19b8e19b09abdf951abc7898078068f5582fe16d
=viraj <=viraj.mandlik@mitaoe.ac.in>
2025-04-12 10:52:44 +0530

    added the fronted public poring in server .js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const app = express();
 
 // CORS configuration
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', 'https://render-frontend-git-main-virajmandliks-projects.vercel.app/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
